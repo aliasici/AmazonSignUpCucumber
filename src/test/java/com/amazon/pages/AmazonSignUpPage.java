0766204ed4f18cc5eb15038697ed07aa8280d981
aliasici <asiciali@yahoo.com>
2021-07-15 10:24:34 -0700

    I added new method

--- src/test/java/com/amazon/pages/AmazonSignUpPage.java
+++ src/test/java/com/amazon/pages/AmazonSignUpPage.java
@@ -1,6 +1,8 @@
 package com.amazon.pages;
 
+import com.amazon.utilities.ConfigurationReader;
 import com.amazon.utilities.Driver;
+import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -25,20 +27,32 @@ public class AmazonSignUpPage {
     public WebElement userName;
 
     @FindBy(id="ap_email")
-    public WebElement email;
+    public WebElement emailValue;
 
     @FindBy(id="ap_password")
-    public WebElement password;
+    public WebElement firstPassword;
 
     @FindBy(id="ap_password_check")
     public WebElement ReEnterPassword;
 
 
-    public void SignUp(String usernameValue, String emailValue,String passwordValue, String check) {
+//    public void SignUp(String usernameValue, String emailValue,String passwordValue, String check) {
+//        userName.sendKeys(usernameValue);
+//        email.sendKeys(emailValue);
+//        password.sendKeys(passwordValue);
+//        ReEnterPassword.sendKeys(check, Keys.ENTER);
+//
+//    }
+    public void SignUp() {
+        String usernameValue = ConfigurationReader.getProperty("userName");
+        String email = ConfigurationReader.getProperty("email");
+        String password = ConfigurationReader.getProperty("password");
+        String passwordValue = ConfigurationReader.getProperty("ReEnterPassword");
+
         userName.sendKeys(usernameValue);
-        email.sendKeys(emailValue);
-        password.sendKeys(passwordValue);
-        ReEnterPassword.sendKeys(check, Keys.ENTER);
+        emailValue.sendKeys(email);
+        firstPassword.sendKeys(password);
+        ReEnterPassword.sendKeys(passwordValue, Keys.ENTER);
 
     }
 
