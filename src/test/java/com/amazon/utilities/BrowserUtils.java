f818601ccc564c4c6f163e6e71311771d32eb45f
aliasici <asiciali@yahoo.com>
2021-07-17 14:13:00 -0700

    I added BrowserUtils with wait method

--- src/test/java/com/amazon/utilities/BrowserUtils.java
+++ src/test/java/com/amazon/utilities/BrowserUtils.java
@@ -1,2 +1,13 @@
-package com.amazon.utilities;public class BrowserUtils {
+package com.amazon.utilities;
+
+public class BrowserUtils {
+
+        public static void wait(int sec){
+            try {
+                Thread.sleep(1000 * sec);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted exception caught");
+            }
+        }
+
 }
